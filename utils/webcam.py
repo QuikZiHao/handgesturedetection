04a989e0851c444de3750a361e9693f1b7f0b2cf
L3us00 <liewziyi@hotmail.com>
2024-07-06 01:43:20 +0800

    Update webcam.py
    put webcam into function

--- utils/webcam.py
+++ utils/webcam.py
@@ -1,23 +1,24 @@
 import cv2
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
+def startVideo():
+    # Initialize the webcam
+    cap = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    
-    # If frame is read correctly ret is True
-    if not ret:
-        print("Failed to grab frame")
-        break
-    
-    # Display the resulting frame
-    cv2.imshow('Webcam', frame)
-    
-    # Break the loop on 'q' key press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        
+        # If frame is read correctly ret is True
+        if not ret:
+            print("Failed to grab frame")
+            break
+        
+        # Display the resulting frame
+        cv2.imshow('Webcam', frame)
+        
+        # Break the loop on 'q' key press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
